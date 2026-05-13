4f56801b004c48701ec24a5a57463ceab4660ea5
Benedict Musila <32413436+ScBencom@users.noreply.github.com>
2026-05-13 07:48:34 +0300

    Testing my committing approach

--- datastructures_in_Java/methodOverloading.java
+++ datastructures_in_Java/methodOverloading.java
@@ -7,10 +7,18 @@ static double plusMethod(double x, double y) {
   return x + y;
 }
 
+static String plusMethod(String firstName, String lastName){
+    return firstName + lastName;
+}
+
 public static void main(String[] args) {
   int myNum1 = plusMethod(8, 5);
   double myNum2 = plusMethod(4.3, 6.26);
+  String myString3 = plusMethod("Benedict", " Musila");
   System.out.println("int: " + myNum1);
   System.out.println("double: " + myNum2);
+  System.out.println("string: " + myString3);
 }
 }
+
+//Testing my commit messages
