5d8b371c6edef8c2e9ab026e179c18f44d696802
Benedict Musila <32413436+ScBencom@users.noreply.github.com>
2026-05-19 11:34:49 +0300

    Using sort method in Java

--- datastructures_in_Java/algorithms/sorting.java
+++ datastructures_in_Java/algorithms/sorting.java
@@ -1,1 +1,16 @@
 //you can use Collections.sort() to sort the elements
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) {
+    ArrayList<Integer> numbers = new ArrayList<>();
+    numbers.add(5);
+    numbers.add(1);
+    numbers.add(7);
+    numbers.add(3);
+    numbers.add(9);
+
+    Collections.sort(numbers);
+    System.out.println(numbers); // [1, 3, 5, 7, 9]
+  }
+}
